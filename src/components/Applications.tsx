ffe03f1443408781574535648004174ebf7f9adc
Alexandru Ciucalau <ciucalau.alexandru@yahoo.com>
2026-04-30 15:39:49 +0300

    Added applications page reference

--- src/components/Applications.tsx
+++ src/components/Applications.tsx
@@ -1,6 +1,7 @@
 
 import { Poppins } from 'next/font/google';
 import {  Syne } from 'next/font/google';
+import Link from 'next/link';
 
 const poppinsFont = Poppins({
   subsets: ["latin"],
@@ -17,18 +18,18 @@ export default function Applications () {
         <div className="py-10 bg-white mx-auto">
           <div className="h-px bg-black"></div>
         </div>
-        <div>
+        <div className=' '>
           {/* Next label */}
           <p className={`text-medium mb-1 mt-4 text-[13px] font-normal ${poppinsFont.className}`}>Next</p>
 
           {/* Applications heading with arrow */}
           <div className="flex items-center justify-between">
             <h4 className={`text-4xl font-semibold text-black tracking-tight text-[30px] ${syneFont.className}`}>Applications</h4>
-            <button className="text-black hover:text-gray-600 transition-colors lg:mr-7 mt-3">
+            <Link href="/applications" className="text-black hover:text-gray-600 transition-colors lg:mr-7 mt-3">
               <svg className="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 21 21">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5l7 7-7 7" />
               </svg>
-            </button>
+            </Link>
           </div>
         </div>
       </div>
