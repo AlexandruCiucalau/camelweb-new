9b0c7177987ab096a4b8e459fbca7025f0c85ec0
AlexandruCiucalau <ciucalau.alexandru@yahoo.com>
2026-09-24 11:40:08 +0300

    Add alt text to ImageSection images
    Previously missing entirely (jsx-a11y/alt-text), leaving these three
    product images inaccessible to screen readers.

--- src/app/applications/components/ImageSection.tsx
+++ src/app/applications/components/ImageSection.tsx
@@ -5,18 +5,22 @@ export default function ImageSection() {
                 <div className="pr-30 pt-27">
                     <div className=" w-[562px] h-[854px] overflow-hidden">
                         <img src="/images/6.png"
+                            alt=""
                             className="w-full h-full object-cover" />
                     </div>
                 </div>
                 <div className="-translate-x-23 ">
                     <div className="w-[714px] h-[1071.8px] overflow-hidden ">
                         <img src="/images/7.png"
+                            alt=""
                             className="w-full h-full object-cover" />
                     </div>
                 </div>
                 <div className="col-span-2">
                     <div className=" w-[1246px] h-[780px] overflow-hidden mx-auto mt-10 mb-52">
-                        <img src="/images/8.png" className="w-full h-full object-cover" />
+                        <img src="/images/8.png" 
+                            alt="" 
+                            className="w-full h-full object-cover" />
                     </div>
                 </div>
             </div>
